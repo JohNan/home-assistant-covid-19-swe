9d381104793200392d2e27e1524fd0d6adf3aa66
Johan Nenzén <johan.nanzen@gmail.com>
2020-03-25 21:16:59 +0100

    Changes data provider url to one that actually updates

--- custom_components/covid_19_swe/covid_19_swe.py
+++ custom_components/covid_19_swe/covid_19_swe.py
@@ -1,6 +1,8 @@
 """Fetch latest COVID-19 cases in Sweden."""
 from aiohttp import ClientSession, ClientResponseError
 from dataclasses import dataclass
+from datetime import datetime
+
 import logging
 
 
@@ -8,23 +10,23 @@ import logging
 class SweRegion:
     """Class for holding country stats."""
 
-    URL = "https://www.svt.se/special/articledata/2322/sverige.json"
-    NAME = "SVT"
+    URL = "https://www.svt.se/special/articledata/2322/folkhalsomyndigheten.json"
+    NAME = "SVT Datajournalistik"
 
     id: str
     region: str
     confirmed: int
     deaths: int
-    updated: int
+    updated: datetime
 
     @staticmethod
-    def from_json(item, last_updated):
+    def from_json(item):
         return SweRegion(
             id=item["kod"],
-            region=item["namn"],
-            confirmed=item["antal"],
-            deaths=item["dead"],
-            updated=last_updated,
+            region=item["region"],
+            confirmed=item["fall"],
+            deaths=item["avlidna"],
+            updated=datetime.fromtimestamp(item["ts"] / 1000),
         )
 
 
@@ -35,9 +37,10 @@ async def get_cases(session: ClientSession, *, source=SweRegion):
 
     results = []
 
-    for item in data["data"]:
+    for item in data:
         try:
-            results.append(source.from_json(item, data["data_updated"]))
+            if item['days'] is 0:
+                results.append(source.from_json(item))
         except KeyError:
             logging.getLogger(__name__).warning("Got wrong data: %s", item)
 
